acd598bb5902e089cdb4dad9f7647ada873831d6
Sergei <gannochenko.sv@gmail.com>
2019-11-13 22:26:58 +0100

    tests

--- packages/core/src/__test__/interpolator.test.ts
+++ packages/core/src/__test__/interpolator.test.ts
@@ -2,6 +2,31 @@ import { Interpolator } from '../interpolator';
 
 describe('Interpolator', () => {
     it('should replace several placeholders', async () => {
-        expect(Interpolator.apply('[one][two][three]', { one: '1', two: '2', three: '3'})).toEqual('123');
+        expect(
+            Interpolator.apply('[one][two][three]', {
+                one: '1',
+                two: '2',
+                three: '3',
+            }),
+        ).toEqual('123');
+    });
+    it('should remove conditional placeholder', async () => {
+        expect(
+            Interpolator.apply('[?one]two', { one: '1', two: '2', three: '3' }),
+        ).toEqual('two');
+        expect(
+            Interpolator.apply('[?one]two', {
+                one: false,
+                two: '2',
+                three: '3',
+            }),
+        ).toEqual('two');
+        expect(
+            Interpolator.apply('[?one][?two]three', {
+                one: false,
+                two: '2',
+                three: '3',
+            }),
+        ).toEqual('three');
     });
 });
