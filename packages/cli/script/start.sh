8355331a4d58c97a656852bdd0ec25b38c5e1a4c
Sergei <gannochenko.sv@gmail.com>
2019-11-13 22:41:49 +0100

    upgrade

--- packages/cli/script/start.sh
+++ packages/cli/script/start.sh
@@ -6,6 +6,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 #GENERILLA_DST=${DIR}/../../../_output yarn start run react.component -a '{"component_name":"ggggg","use_tests":true}' -y -o ${DIR}/../../../_output2;
 #GENERILLA_DST=${DIR}/../../../_output yarn start run react.component -y -m
 #GENERILLA_DST=${DIR}/../../../_output yarn start -m
-GENERILLA_DST=${DIR}/../../../_output yarn start list -d
+GENERILLA_DST=${DIR}/../../../_output yarn start
 #GENERILLA_DST=${DIR}/../../../_output yarn start;
 #GENERILLA_DST=${DIR}/../../../_output yarn start -h;
